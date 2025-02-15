b12a8828dbee5e91bf8633484700f472e088e020
Andrew <myuan09@nyit.edu>
2025-02-14 23:49:48 -0800

    refactor: change whole project

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,85 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <div id="app" >
-    <router-view/>
-  </div>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
 
-<script>
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
 
-import DwHeader from '@/components/layouts/DwHeader.vue'
-import DwHeaderLogin from '@/components/layouts/DwHeaderLogin.vue'
-import DwNavMenu from '@/components/layouts/DwNavMenu.vue'
-import DwFooter from '@/components/layouts/DwFooter.vue'
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
 
-export default {
-  name: 'App',
-  components: {
-    'dw-nav-menu': DwNavMenu,
-    'dw-header': DwHeader,
-    'dw-header-login': DwHeaderLogin,
-    'dw-footer': DwFooter
-  },
-  data () {
-    return {
-      currentHeader: 'dw-header'
-    }
-  },
-  mounted () {
-    this.loginStatus()
-  },
-  methods: {
-    loginStatus: function () {
-      const fullPath = this.$route.fullPath
-      if (fullPath.indexOf('login') >= 0) {
-        this.currentHeader = 'dw-header-login'
-      } else {
-        this.currentHeader = 'dw-header'
-      }
-    }
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
   }
-}
-</script>
-<style>
-body{
-  background-color: #F1F1F1;
-  /*#ffb25c*/
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-.el-header{
-  background-color: #25292E;
-}
-.el-footer {
-  /*background-color: #B3C0D1;*/
-  line-height: 60px;
-  color: #7c7c7c;
-}
-.el-aside {
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
